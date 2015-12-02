bbfb6b945ea30f01d295612850f087182ce7953f
Ondrej Hubacek <ondrej.hubacek@gmail.com>
2015-12-02 14:00:34 +0100

    deleted unused functions

--- lib/datasets/eshops.py
+++ lib/datasets/eshops.py
@@ -24,7 +24,7 @@ class eshops(datasets.imdb):
     def __init__(self, image_set, data_path=None):
         datasets.imdb.__init__(self, 'eshops')
         self._image_set = image_set
-        self._data_path = os.path.join(datasets.ROOT_DIR, 'data', 'new_eshops')
+        self._data_path = os.path.join(datasets.ROOT_DIR, 'data', 'eshops')
         self._classes = ('__background__', 'price', 'main_image', 'name')
         self._class_to_ind = dict(zip(self.classes, xrange(self.num_classes)))
         self._image_ext = '.jpeg'
@@ -132,45 +132,6 @@ class eshops(datasets.imdb):
 
         return self.create_roidb_from_box_list(box_list, gt_roidb)
 
-	#TODO REMOVE?
-    def selective_search_IJCV_roidb(self):
-        """
-        Return the database of selective search regions of interest.
-        Ground-truth ROIs are also included.
-
-        This function loads/saves from/to a cache file to speed up future calls.
-        """
-        cache_file = os.path.join(self.cache_path, '{:s}_selective_search_IJCV_top_{:d}_roidb.pkl'. format(self.name, self.config['top_k']))
-
-        if os.path.exists(cache_file):
-            with open(cache_file, 'rb') as fid:
-                roidb = cPickle.load(fid)
-            print '{} ss roidb loaded from {}'.format(self.name, cache_file)
-            return roidb
-
-        gt_roidb = self.gt_roidb()
-        ss_roidb = self._load_selective_search_IJCV_roidb(gt_roidb)
-        roidb = datasets.imdb.merge_roidbs(gt_roidb, ss_roidb)
-        with open(cache_file, 'wb') as fid:
-            cPickle.dump(roidb, fid, cPickle.HIGHEST_PROTOCOL)
-        print 'wrote ss roidb to {}'.format(cache_file)
-
-        return roidb
-
-	#TODO REMOVE?
-    def _load_selective_search_IJCV_roidb(self, gt_roidb):
-        IJCV_path = os.path.abspath(os.path.join(self.cache_path, '..', 'selective_search_IJCV_data', 'voc_' + self._year))
-        assert os.path.exists(IJCV_path), 'Selective search IJCV data not found at: {}'.format(IJCV_path)
-
-        top_k = self.config['top_k']
-        box_list = []
-        for i in xrange(self.num_images):
-            filename = os.path.join(IJCV_path, self.image_index[i] + '.mat')
-            raw_data = sio.loadmat(filename)
-            box_list.append((raw_data['boxes'][:top_k, :]-1).astype(np.uint16))
-
-        return self.create_roidb_from_box_list(box_list, gt_roidb)
-
     def _load_annotation(self, index):
         """
         Load image and bounding boxes info from XML file in the PASCAL VOC
@@ -208,52 +169,31 @@ class eshops(datasets.imdb):
                     'gt_overlaps' : overlaps,
                     'flipped' : False}
 
-	#TODO
-    def _write_voc_results_file(self, all_boxes):
-        #use_salt = self.config['use_salt']
-        use_salt = False
-        comp_id = 'comp4'
-        if use_salt:
-            comp_id += '-{}'.format(os.getpid())
+    def _write_results_file(self, all_boxes):
+        comp_id = '-{}'.format(os.getpid())
 
         # VOCdevkit/results/VOC2007/Main/comp4-44503_det_test_aeroplane.txt
-        path = os.path.join(self._data_path, comp_id + '_')
+        path = os.path.join(self._data_path, 'results')
         for cls_ind, cls in enumerate(self.classes):
             if cls == '__background__':
                 continue
-            print 'Writing {} VOC results file'.format(cls)
-            filename = path + 'det_' + self._image_set + '_' + cls + '.txt'
+            print 'Writing {} results file'.format(cls)
+            filename = os.path.join(path, self._image_set + comp_id + '.txt')
             with open(filename, 'wt') as f:
                 for im_ind, index in enumerate(self.image_index):
                     dets = all_boxes[cls_ind][im_ind]
                     if dets == []:
                         continue
-                    # the VOCdevkit expects 1-based indices
                     for k in xrange(dets.shape[0]):
                         f.write('{:s} {:.3f} {:.1f} {:.1f} {:.1f} {:.1f}\n'.
                                 format(index, dets[k, -1],
-                                       dets[k, 0] + 1, dets[k, 1] + 1,
-                                       dets[k, 2] + 1, dets[k, 3] + 1))
+                                       dets[k, 0], dets[k, 1],
+                                       dets[k, 2], dets[k, 3]))
         return comp_id
 
-	#TODO
-    def _do_matlab_eval(self, comp_id, output_dir='output'):
-        #rm_results = self.config['cleanup']
-        rm_results = False
-
-        path = os.path.join(os.path.dirname(__file__), 'VOCdevkit-matlab-wrapper')
-        cmd = 'cd {} && '.format(path)
-        cmd += '{:s} -nodisplay -nodesktop '.format(datasets.MATLAB)
-        cmd += '-r "dbstop if error; '
-        cmd += 'voc_eval(\'{:s}\',\'{:s}\',\'{:s}\',\'{:s}\',{:d}); quit;"' \
-               .format(self._data_path, comp_id,
-                       self._image_set, output_dir, int(rm_results))
-        print('Running:\n{}'.format(cmd))
-        #status = subprocess.call(cmd, shell=True)
 
     def evaluate_detections(self, all_boxes, output_dir):
-        comp_id = self._write_voc_results_file(all_boxes)
-        #self._do_matlab_eval(comp_id, output_dir)
+        comp_id = self._write_results_file(all_boxes)
 
 if __name__ == '__main__':
     #d = datasets.eshops('all')
